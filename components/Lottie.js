159f462c461aa1027904cf54864f6d724812f63c
Ashish Sharma <developer.ashish18@gmail.com>
2021-12-15 19:38:25 +0530

    Add neccessary assets.

---
+++ components/Lottie.js
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from 'react';
+import lottie from 'lottie-web';
+
+const Lottie = ({ name }) => {
+  const container = useRef(null);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require(`../lotties/${name}`),
+    });
+  }, [name]);
+  return <div ref={container}></div>;
+};
+
+export default Lottie;
