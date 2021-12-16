f3b680abf7f6b725537401089cdaed7016ebe461
Ashish Sharma <developer.ashish18@gmail.com>
2021-12-16 10:39:48 +0530

    Built the Navbar component (full).

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,24 +7,72 @@ import Logo from '../assets/logos/atomic-programmer.svg';
 // Icons
 import { PlusSmIcon } from '@heroicons/react/outline';
 
+// Disclosure
+import { Disclosure } from '@headlessui/react';
+
+const navigation = [
+  { name: 'Inside the atom', href: '/about' },
+  { name: 'Projects', href: '/projects' },
+  { name: 'Designs', href: '/designs' },
+  { name: 'Tools', href: '/tools' },
+  { name: 'Why Us?', href: '/why-us' },
+  { name: 'Say Hello!', href: '/message' },
+];
+
 const Navbar = () => {
   return (
-    <div className='pt-6 flex items-center justify-between'>
-      <Link href='/'>
-        <a className='pt-1.5'>
-          <Image
-            src={Logo}
-            alt='Atomic Programmers Logo'
-            height={32.1}
-            width={171.6}
-          />
-        </a>
-      </Link>
+    <Disclosure as='nav'>
+      {({ open }) => (
+        <>
+          <div className='pt-6 lg:pt-8'>
+            <div className='flex items-center justify-between'>
+              <Link href='/'>
+                <a className='pt-1.5'>
+                  <Image src={Logo} alt='Logo' width={171.6} height={32.1} />
+                </a>
+              </Link>
+              <div className='flex items-center lg:hidden'>
+                <Disclosure.Button className='text-slate-500 hover:text-slate-600'>
+                  <span className='sr-only'>Open main menu</span>
+                  {open ? (
+                    <PlusSmIcon className='block h-6 w-6' aria-hidden='true' />
+                  ) : (
+                    <PlusSmIcon className='block h-6 w-6' aria-hidden='true' />
+                  )}
+                </Disclosure.Button>
+              </div>
 
-      <button className='w-8 h-8 text-slate-500 hover:text-slate-600'>
-        <PlusSmIcon className='8789439998' />
-      </button>
-    </div>
+              <nav className='hidden lg:flex'>
+                <div className='flex lg:gap-x-6'>
+                  {navigation.map((item) => (
+                    <Link key={item.name} href={item.href}>
+                      <a className='text-sm font-semibold text-slate-700 hover:text-sky-500'>
+                        {item.name}
+                      </a>
+                    </Link>
+                  ))}
+                </div>
+              </nav>
+            </div>
+          </div>
+
+          <Disclosure.Panel className='lg:hidden'>
+            <div className='px-2 pt-6 pb-3 flex flex-col gap-y-3'>
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as='a'
+                  href={item.href}
+                  className='text-sm font-semibold text-slate-700 hover:text-sky-500'
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 };
 
--- sections/Hero.js
+++ sections/Hero.js
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar';
 
 const Hero = () => {
   return (
-    <header className='bg-slate-50 px-4'>
+    <header className='bg-slate-50 px-4 md:px-6 lg:px-8'>
       <Navbar />
     </header>
   );
