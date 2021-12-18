63a0d5c53d150ad0c14826017e2777fd09b63dd3
Ashish Sharma <developer.ashish18@gmail.com>
2021-12-18 11:40:14 +0530

    Hover behaviour deactivated temp.

--- Tabs/ProjectsTab.js
+++ Tabs/ProjectsTab.js
@@ -4,11 +4,12 @@ const ProjectsTab = ({ collection }) => {
   return (
     <a href={collection.to} target='_blank' rel='noopener noreferrer'>
       <Image
+        // className='transition-all ease-out duration-300 hover:brightness-75 hover:scale-95'
+        priority
         src={collection.img}
         alt={collection.name}
         width={1080}
         height={615}
-        className='rounded-lg transition-all ease-out duration-300 hover:brightness-75 hover:scale-95'
       />
     </a>
   );
